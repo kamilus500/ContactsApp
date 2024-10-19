c934fda3f4623e8adac5ffb0209bf0497c5ec44b
kamilus500 <kkurzeja321@gmail.com>
2024-10-19 09:01:45 +0100

    remvoe bad created angular project

--- ContactsApp.UI/src/app/models/ContactDto.ts
+++
@@ -1,7 +0,0 @@
-export interface ContactDto {
-    id: string,
-    firstName: string,
-    lastName: string,
-    email: string,
-    numberPhone: string
-}
--- ContactsApp.UI/src/app/models/LoginRegisterDto.ts
+++
@@ -1,6 +0,0 @@
-export interface LoginRegisterDto {
-    email: string,
-    firstName: string,
-    lastName: string,
-    password: string
-}
