5b60f51d07fce44065414cf7f0921f83d42ddcf9
kamilus500 <kkurzeja321@gmail.com>
2024-10-26 15:46:43 +0100

    remove columns

--- ContactsApp.Domain/Entities/User.cs
+++ ContactsApp.Domain/Entities/User.cs
@@ -4,8 +4,6 @@ namespace ContactsApp.Domain.Entities
 {
     public class User : IdentityUser
     {
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
         public IEnumerable<Contact> Contacts { get; set; }
     }
 }
