95d8d2a71cfdba14258f20072aa664c8707f24fe
kamilus500 <kkurzeja321@gmail.com>
2024-11-11 21:04:41 +0100

    remove connectionstring in program.cs

--- ContactsApp.API/Program.cs
+++ ContactsApp.API/Program.cs
@@ -3,8 +3,6 @@ using ContactsApp.Infrastructure.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var connectionString = builder.Configuration.GetConnectionString("connectionString") ?? throw new ArgumentNullException("Connection string is empty");
-
 builder.Services.AddInfrastructure(builder);
 builder.Services.AddApplication();
 
