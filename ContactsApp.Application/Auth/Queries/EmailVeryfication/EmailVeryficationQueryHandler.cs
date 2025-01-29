6f68fddd8c6aaa0fa715e0f945a43c46ffbd9bd7
kamilus500 <kkurzeja321@gmail.com>
2025-01-29 20:31:08 +0100

    fix tests

--- ContactsApp.Application/Auth/Queries/EmailVeryfication/EmailVeryficationQueryHandler.cs
+++ ContactsApp.Application/Auth/Queries/EmailVeryfication/EmailVeryficationQueryHandler.cs
@@ -1,6 +1,7 @@
 ﻿using ContactsApp.Application.BaseClasses;
 using ContactsApp.Domain.Interfaces;
 using MediatR;
+using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.Logging;
 
 namespace ContactsApp.Application.Auth.Queries.EmailVeryfication
@@ -8,7 +9,7 @@ namespace ContactsApp.Application.Auth.Queries.EmailVeryfication
     public class EmailVeryficationQueryHandler : BaseHandler<EmailVeryficationQueryHandler>, IRequestHandler<EmailVeryficationQuery, bool>
     {
         private readonly IUserRepository _userRepository;
-        public EmailVeryficationQueryHandler(ILogger<EmailVeryficationQueryHandler> logger, IUserRepository userRepository) : base(null, logger)
+        public EmailVeryficationQueryHandler(ILogger<EmailVeryficationQueryHandler> logger, IMemoryCache memoryCache, IUserRepository userRepository) : base(memoryCache, logger)
         {
             _userRepository = userRepository ?? throw new ArgumentNullException(nameof(userRepository));
         }
